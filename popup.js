0a0bc2dc5419429ef9d72d6aa0b65b4b9ce2a2c3
Tim <timmy>
2018-10-11 20:59:40 +0100

    Save tabs to popout

--- popup.js
+++ popup.js
@@ -13,9 +13,27 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
   let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+  let openTabs = [];
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+    	let tab = tabs[i];
+    	openTabs.push({iconUrl: tab.favIconUrl, url: tab.url, title: tab.title});	
+    }
+    let savedTabs = document.getElementById('savedTabs');
+	for (var i = 0; i < openTabs.length; i++) {
+		let tab = openTabs[i];
+		let tabEl = document.createElement("div");
+		if (tab.iconUrl) {
+			tabEl.style = "height: 50px; width: 50px; background-image: url('" + tab.iconUrl + "'); background-size: contain; display: inline-block";	
+		}
+		else {
+			tabEl.style = "height: 50px; width: 50px; background: black; display: inline-block";	
+		}
+
+		savedTabs.appendChild(tabEl);
+	};
+
+    chrome.storage.sync.set({openTabs: openTabs}, function() {	
+    });
+});
+}
