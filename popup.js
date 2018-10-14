e5ff4dc3985a17ddeafd6f3e3c1596311771efee
Tim <timmy>
2018-10-14 11:08:35 +0100

    Add functionality to restore whole session

--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ let createNewBtn = document.getElementById('createNew');
 createNewBtn.onclick = function(element) {
   let name = document.getElementById('createNewName').value;
   
-  chrome.storage.sync.get("savedTabSets", function(data) {
+  chrome.storage.local.get("savedTabSets", function(data) {
   	chrome.tabs.query({currentWindow: true}, function(tabs) {
   		let currentSavedSets = data.savedTabSets || [];
   		let currentOpenTabs = [];
@@ -24,7 +24,8 @@ createNewBtn.onclick = function(element) {
 	    var obj = {name: name, tabs: currentOpenTabs};
 	    currentSavedSets.push(obj);
 	    renderTabSet(obj);
-	    chrome.storage.sync.set({savedTabSets: currentSavedSets});
+	    chrome.storage.local.set({savedTabSets: currentSavedSets});
+	    document.getElementById("createNewName").value = "";
 	});	
   });
 }
@@ -44,21 +45,30 @@ function renderTabSet(tabSet) {
 	let tabWidth = 30;
 	let tabMargin = 5;
 	let tabSetEl = document.createElement("div");
-	let titleEl = document.createElement("h3");
+	let titleEl = document.createElement("div");
 	titleEl.textContent = name;
 	tabSetEl.appendChild(titleEl);
 
-	let deleteEl = document.createElement("div");
+	let restoreEl = document.createElement("button");
+	restoreEl.setAttribute("class", "restoreBtn");
+	restoreEl.setAttribute("data-name", name);
+	restoreEl.style = "cursor: pointer";
+	restoreEl.textContent = "restore";
+	restoreEl.addEventListener("click", restoreSet);
+	titleEl.appendChild(restoreEl);
+
+	let deleteEl = document.createElement("button");
 	deleteEl.setAttribute("class", "deleteBtn");
 	deleteEl.setAttribute("data-name", name);
+	deleteEl.style = "cursor: pointer";
 	deleteEl.textContent = "delete";
 	deleteEl.addEventListener("click", deleteSet);
-	tabSetEl.appendChild(deleteEl);
+	titleEl.appendChild(deleteEl);
 
 	for (var i = 0; i < tabs.length; i++) {
 		let tab = tabs[i];
 		let tabEl = document.createElement("div");
-		tabEl.style = "display: inline-block; padding-left: " + tabMargin + "px; padding-right: " + tabMargin + "px; text-align: center; border-right: 1px solid #ccc;";
+		tabEl.style = "display: inline-block; padding-left: " + tabMargin + "px; padding-right: " + tabMargin + "px; text-align: center; border-right: 1px solid #ccc; cursor: pointer;";
 		let tabImage = document.createElement("div");
 		tabImage.setAttribute("title", tab.title)
 		if (tab.iconUrl) {
@@ -71,7 +81,9 @@ function renderTabSet(tabSet) {
 		let tabDescription = document.createElement("div");
 		tabDescription.style = "overflow: hidden; font-size: 10px;"
 		tabDescription.textContent = tab.title.substring(0, 8);
+		tabEl.setAttribute("data-url", tab.url);
 		tabEl.appendChild(tabDescription);
+		tabEl.addEventListener("click", openIndividualLink);
 		tabSetEl.appendChild(tabEl);
 	};
 	savedTabs.appendChild(tabSetEl);
@@ -85,31 +97,53 @@ function renderTabSet(tabSet) {
 	}
 }
 
+function openIndividualLink(e) {
+	var url = e.target.getAttribute("data-url");
+	if (!url) {
+		openIndividualLink({target: e.target.parentElement});
+		return;
+	}
+	chrome.tabs.create({url: url});
+}
+
 function deleteSet(e) {
 	var name = e.target.getAttribute("data-name");
 	var setEl = e.target.parentElement;
 	setEl.parentNode.removeChild(setEl);
-	chrome.storage.sync.get('savedTabSets', function(data) {
+	chrome.storage.local.get('savedTabSets', function(data) {
 		var savedTabSets = data.savedTabSets;
 		for (var i = 0; i < savedTabSets.length; i++) {
 			if (savedTabSets[i].name === name) {
 				savedTabSets.splice(i, 1);
-				chrome.storage.sync.set({savedTabSets: savedTabSets});
+				chrome.storage.local.set({savedTabSets: savedTabSets});
 				return;
 			}
 		}
 	})
 }
 
+function restoreSet(e) {
+	var name = e.target.getAttribute("data-name");
+	var setEl = e.target.parentElement;
+	chrome.storage.local.get('savedTabSets', function(data) {
+		var savedTabSets = data.savedTabSets;
+		for (var i = 0; i < savedTabSets.length; i++) {
+			if (savedTabSets[i].name === name) {
+				var urls = savedTabSets[i].tabs.map(x => x.url);
+				chrome.windows.create({
+					type: "normal",
+					state: "maximized",
+					url: urls
+				})
+			}
+		}
+	})
+}
+
 window.onload = function() {
-	chrome.storage.sync.get('savedTabSets', function(data) {
+	chrome.storage.local.get('savedTabSets', function(data) {
 		if (data.savedTabSets && data.savedTabSets.length) {
-			for (var i = 0; i < data.savedTabSets.length; i++) {
-				var id = data.savedTabSets[i];
-				chrome.storage.sync.get(id, function(data) {
-					renderTabSets(data[id]);	
-				});
-			};
+			renderTabSets(data.savedTabSets);	
 		};
 	});
 }
